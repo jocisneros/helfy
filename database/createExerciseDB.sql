060b3ba3a155e66a37ea1c2cefe668b7e686b3c4
Aayush Bokil <aayushbokil@aayushs-mbp.lan>
2023-02-23 05:07:07 -0800

    Fixed SQL syntax

--- database/createExerciseDB.sql
+++ database/createExerciseDB.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS exercises (
 	id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     difficulty INT NOT NULL,
-    tips VARCHAR(500) NOT NULL,
-    link VARCHAR(100) NOT NULL,
+    tips VARCHAR(1200) NOT NULL,
+    link VARCHAR(500) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -25,8 +25,8 @@ CREATE TABLE IF NOT EXISTS workout_types (
 CREATE TABLE IF NOT EXISTS muscles_in_workout_types (
     muscleID INT NOT NULL,
 	workoutTypeID INT NOT NULL,
-    FOREIGN KEY (muscleID) REFERENCES muscle_groups(id)
-     FOREIGN KEY (workoutTypeID) REFERENCES workout_types(id)
+    FOREIGN KEY (muscleID) REFERENCES muscle_groups(id),
+	FOREIGN KEY (workoutTypeID) REFERENCES workout_types(id)
 );
 
 CREATE TABLE IF NOT EXISTS muscle_groups_in_exercises (
