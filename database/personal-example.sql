0fbd0532d4e2af937be72105bbca9f81d50799d4
Akhil Golla <agolla@uci.edu>
2023-03-12 00:46:27 -0800

    update example data

--- database/personal-example.sql
+++ database/personal-example.sql
@@ -1,52 +1,33 @@
 USE personaldb;
 
 # users (id, name, height-inches, weight-pounds, gender, experience)
-INSERT INTO users VALUES('abc0', 'Jane Doe', 65, 150, 'female', 0);
-INSERT INTO users VALUES('abc1', 'Jimmy Neutron', 75, 145, 'male',1);
-INSERT INTO users VALUES('abc2', 'Johnny Bravo', 74, 215, 'male', 2);
-INSERT INTO users VALUES('abc3', 'Peter Griffin', 79, 205, 'male', 0);
+# example user
+INSERT INTO users VALUES('abc0', 'Example User', 75, 190, 'male', 0);
+
+# example users
+INSERT INTO users VALUES('abc1', 'Jimmy Neutron', 75, 145, 'male', 0);
+INSERT INTO users VALUES('abc2', 'Johnny Bravo', 74, 185, 'male', 0);
+INSERT INTO users VALUES('abc3', 'Peter Griffin', 79, 175, 'male', 0);
 INSERT INTO users VALUES('abc4', 'Velma Dinkley', 63, 140, 'female', 0);
-INSERT INTO users VALUES('abc5', 'Shaggy Rogers', 78, 185, 'male', 2);
+INSERT INTO users VALUES('abc5', 'Jane Doe', 67, 125, 'female', 0);
+
+# additional users, not given workouts
+
 INSERT INTO users VALUES('abc6', 'Fred Jones', 76, 180, 'male', 1);
-INSERT INTO users VALUES('abc7', 'Daphne Blake', 65, 130, 'female', 0);
-INSERT INTO users VALUES('abc6', 'Ned Flanders', 75, 175, 'male', 1);
-INSERT INTO users VALUES('abc7', 'Marge Simpson', 56, 125, 'female', 0);
-INSERT INTO users VALUES('abc8', 'Big Chungus', 80, 250, 'other', 0);
-INSERT INTO users VALUES('abc9', 'The Potato', 51, 95, 'other', 2);
+INSERT INTO users VALUES('abc7', 'Ned Flanders', 75, 175, 'male', 1);
+INSERT INTO users VALUES('abc8', 'The Potato', 51, 95, 'other', 2);
+INSERT INTO users VALUES('abc9', 'Shaggy Rogers', 78, 185, 'male', 2);
 
-# workout_history (id, usersID, workoutDate, workoutType)
-INSERT INTO workout_history VALUES (1, 'abc0', '2023-01-01', 7)
-INSERT INTO workout_history VALUES (2, 'abc0', '2023-01-02', 7)
-
-INSERT INTO workout_history VALUES (3, 'abc1', '2023-01-02', 7)
-INSERT INTO workout_history VALUES (4, 'abc1', '2023-01-03', 7)
-
-INSERT INTO workout_history VALUES (5, 'abc2', '2023-01-01', 7)
-INSERT INTO workout_history VALUES (6, 'abc2', '2023-01-02', 7)
-
-INSERT INTO workout_history VALUES (7, 'abc3', '2023-01-02', 7)
-INSERT INTO workout_history VALUES (8, 'abc3', '2023-01-03', 7)
-
-INSERT INTO workout_history VALUES (9, 'abc4', '2023-01-01', 7)
-INSERT INTO workout_history VALUES (10, 'abc4', '2023-01-02', 7)
-
-INSERT INTO workout_history VALUES (11, 'abc5', '2023-01-02', 7)
-INSERT INTO workout_history VALUES (12, 'abc5', '2023-01-03', 7)
-
-INSERT INTO workout_history VALUES (13, 'abc6', '2023-01-01', 7)
-INSERT INTO workout_history VALUES (14, 'abc6', '2023-01-02', 7)
-
-INSERT INTO workout_history VALUES (15, 'abc7', '2023-01-02', 7)
-INSERT INTO workout_history VALUES (16, 'abc7', '2023-01-03', 7)
-
-INSERT INTO workout_history VALUES (17, 'abc8', '2023-01-01', 7)
-INSERT INTO workout_history VALUES (18, 'abc8', '2023-01-02', 7)
-
-INSERT INTO workout_history VALUES (19, 'abc9', '2023-01-02', 7)
-INSERT INTO workout_history VALUES (20, 'abc9', '2023-01-03', 7)
+# workout_history (usersID, workoutDate, workoutType)
+INSERT INTO workout_history VALUES (1, 'abc1', '2023-01-01', 7)
+INSERT INTO workout_history VALUES (2, 'abc2', '2023-01-02', 7)
+INSERT INTO workout_history VALUES (3, 'abc3', '2023-01-03', 7)
+INSERT INTO workout_history VALUES (4, 'abc4', '2023-01-04', 7)
+INSERT INTO workout_history VALUES (5, 'abc5', '2023-01-05', 7)
 
 
-# exercise_history (id, usersID, exerciseID, workoutID, sets, reps, weight, lengthOfTime, exerciseName)
+# exercise_history (usersID, exerciseID, workoutID, sets, reps, weight, lengthOfTime, exerciseName)
+# for examples: choose 6 beginner exercises and add 4 of the 6 to each workout
 
 #bicep
 
@@ -62,7 +43,15 @@ INSERT INTO workout_history VALUES (20, 'abc9', '2023-01-03', 7)
 
 #lats
 
+##
 #hamstrings
+-- '372','Machine Hamstring Curl'
+-- '374','Kettlebell Deadlift'
+-- '377','Kettlebell Staggered Deadlift'
+-- '378','Hamstrings Stretch Variation Four'
+-- '379','Hamstrings Stretch Variation Three'
+-- '380','Hamstrings Stretch Variation Two'
+
 
 #glutes
 
